5af87cb5cf1c1b8b0feb604a9cd94877a840cd0e
Oleksii Shnepov <shnax0210@gmail.com>
2025-05-24 17:45:15 +0300

    Added --includeFullText to return document full text instead of matched chunks, just another search strategy, it looks less effective than default one, but maybe usefull in some cases

--- collection_search_mcp_stdio_adapter.py
+++ collection_search_mcp_stdio_adapter.py
@@ -10,8 +10,11 @@ mcp = FastMCP("documents-search")
 ap = argparse.ArgumentParser()
 ap.add_argument("-collection", "--collection", required=True, help="collaction name (will be used as root folder name)")
 ap.add_argument("-index", "--index", required=False, default="indexer_FAISS_IndexFlatL2__embeddings_all-MiniLM-L6-v2", help="index that will be used for search")
+
 ap.add_argument("-maxNumberOfChunks", "--maxNumberOfChunks", required=False, type=int, default=100, help="Max number of text chunks in result")
 ap.add_argument("-maxNumberOfDocuments", "--maxNumberOfDocuments", required=False, type=int, default=None, help="Max number of documents in result")
+
+ap.add_argument("-includeFullText", "--includeFullText", action="store_true", required=False, default=False, help="If passed - full text content will be included in the search result. By default only matched chunks content is included. If passed, it's better to reduce --maxNumberOfChunks or set small --maxNumberOfDocuments like 10-30 to avoid too big response and breacking AI agent.")
 args = vars(ap.parse_args())
 
 searcher = create_collection_searcher(collection_name=args['collection'], index_name=args['index'])
@@ -23,8 +26,9 @@ Each document contains 'url' field, if you consider a document as relevant to th
 def search_documents(query: str) -> str:
     search_results = searcher.search(query, 
                                      max_number_of_chunks=args['maxNumberOfChunks'], 
-                                     max_number_of_documents=args['maxNumberOfDocuments'], 
-                                     include_matched_chunks_content=True)
+                                     max_number_of_documents=args['maxNumberOfDocuments'],
+                                     include_text_content=args['includeFullText'],
+                                     include_matched_chunks_content=not args['includeFullText'])
 
     return json.dumps(search_results, indent=4)
     
